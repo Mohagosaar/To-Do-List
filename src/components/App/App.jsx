da1da54d179b401c70b5e1aa8e5a3b430b3beb61
Moha Ali <Moha.gosaar@gmail.com>
2024-04-06 16:29:29 -0500

    To Do list App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,24 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-function App () {
-  
+function App() {
   return (
-    <div>
-      <h1>TO DO APP</h1>
+    <div className="to-do-list">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png"></img>
+        </div>
+        <div className="title">
+          <h1>To Do List app</h1>
+        </div>
+      </nav>
+      <div className="task-registration">
+        <form>
+          <input type="text" placeholder="Enter Task"></input>
+          <input type="submit" value="Add Task" />
+        </form>
+      </div>
     </div>
   );
-
 }
 
-export default App
+export default App;
